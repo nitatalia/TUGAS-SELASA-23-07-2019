b57ccee8aa5651cf948de41cc635170f30868db6
nitatalia <53255248+nitatalia@users.noreply.github.com>
2019-07-24 16:50:08 +0700

    Nitatalia

---
+++ Talianita.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+ // Check Examples
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+ 	public static void main(String args[])
+ 	{ 
+		for(int a=1;a<=22;a++){
+			for(int b=1;b<=a;b++){
+			System.out.print("*");
+		}
+		   System.out.println();
+ 	}
+ }
+}
